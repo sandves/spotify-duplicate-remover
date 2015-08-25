393b8f22a5af2acb923a7410f47b1f9c13e1cf76
Stian Sandve <stian@sandve.org>
2015-08-26 01:02:34 +0200

    Converting to Bootstrap

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,11 +1,22 @@
-angular
+var app = angular
 .module('spoti_dup', ['spotify'])
 .config(function (SpotifyProvider) {
   SpotifyProvider.setClientId('48e5a9cd2b0a481c940b2bf1c3ef9ee5');
-  SpotifyProvider.setRedirectUri('file:///Users/stian/dev/angular/spotify-duplicate-remover/callback.html');
+  SpotifyProvider.setRedirectUri('http://localhost:63342/spotify-duplicate-remover/callback.html');
   SpotifyProvider.setScope('playlist-read-private playlist-modify-private playlist-modify-public');
 })
-.controller('MainController', ['$scope', 'Spotify', function ($scope, Spotify) {
+.controller('MainController', ['$scope', '$http', '$q', 'Spotify', function ($scope, $http, $q, Spotify) {
+
+  $scope.appTitle = "Spotify Duplicate Remover";
+  $scope.appHeadline = "Remove duplicate tracks from your Spotify playlists";
+
+  $scope.currentPage = 0;
+  $scope.pageSize = 20;
+  $scope.playlists = [];
+
+  $scope.numberOfPages=function(){
+      return Math.ceil($scope.playlists.length/$scope.pageSize);
+  }
 
   $scope.searchArtist = function () {
     Spotify.search($scope.searchartist, 'artist', {"limit": 50}).then(function (data) {
@@ -13,126 +24,101 @@ angular
     });
   };
 
+  $scope.loggedIn = false;
+  $scope.userName = "";
+  $scope.token = "";
+  $scope.currentUser = null;
+
   $scope.login = function () {
     Spotify.login().then(function (data) {
       console.log(data);
-      alert("You are now logged in");
+      $scope.loggedIn = true;
+      $scope.token = data;
+      $scope.getCurrentUser();
+      $scope.$watch('currentUser', function(newValue, oldValue) {
+        if (oldValue === null && newValue !== null) {
+          $scope.userName = $scope.currentUser["display_name"];
+          $scope.getUserOwnedPlaylists();
+        }
+      });
+      //alert("You are now logged in");
     }, function () {
       console.log('didn\'t log in');
     });
   };
 
-  $scope.appTitle = "Spotify Duplicate Remover";
-  $scope.appHeadline = "Remove duplicate tracks from your Spotify playlists";
-  $scope.saved = localStorage.getItem('todos');
-  $scope.todos = (localStorage.getItem('todos')!==null) ? JSON.parse($scope.saved) : [ {text: 'Learn AngularJS', done: false}, {text: 'Build an Angular app', done: false} ];
-  localStorage.setItem('todos', JSON.stringify($scope.todos));
+  $scope.setPlaylistTracks = function (playlist_id) {
+      $scope.getPlaylistTracks(playlist_id).then(function (tracks) {
+          $scope.tracks = tracks;
+      });
+  }
 
-  $scope.addTodo = function() {
-    $scope.todos.push({
-      text: $scope.todoText,
-      done: false
-    });
-    $scope.todoText = ''; //clear the input after adding
-    localStorage.setItem('todos', JSON.stringify($scope.todos));
-  };
+  $scope.getPlaylistTracks = function (playlist_id) {
 
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo){
-      count+= todo.done ? 0 : 1;
-    });
-    return count;
-  };
+    return $q(function (resolve, reject) {
+        Spotify.getPlaylistTracks($scope.currentUser["id"], playlist_id).then(function(data) {
+            console.log(data);
 
-  $scope.archive = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo){
-      if (!todo.done)
-        $scope.todos.push(todo);
-    });
-    localStorage.setItem('todos', JSON.stringify($scope.todos));
-  };
+            var tracks = data.items;
 
-    // Gets an album
-    Spotify.getAlbum('0sNOF9WDwhWunNAHPD3Baj').then(function (data){
-      console.log('=================== Album - ID ===================');
-      console.log(data);
-    });
-    // Works with Spotify uri too
-    Spotify.getAlbum('spotify:album:0sNOF9WDwhWunNAHPD3Baj').then(function (data){
-      console.log('=================== Album - Spotify URI ===================');
-      console.log(data);
+            if(data.next != null) {
+                console.log("More than one page");
+                console.log(data.next);
+                $http.get(data.next, {
+                    headers: {'Authorization': "Bearer " + $scope.token}
+                }).then(function(response) {
+                    console.log(response.status);
+                    console.log(response.data);
+                    tracks.push.apply(tracks, response.data.items);
+                }, function(response) {
+                    console.log(response.data || "Request failed");
+                    console.log(response.status);
+                })
+            }
+
+            resolve(tracks);
+
+        }, function(reason) {
+            reject(reason);
+        })
     });
 
-    //Get multiple Albums
-    Spotify.getAlbums('41MnTivkwTO3UUJ8DrqEJJ,6JWc4iAiJ9FjyK0B59ABb4,6UXCm6bOO4gFlDQZV5yL37').then(function (data) {
-      console.log('=================== Albums - Ids ===================');
-      console.log(data);
-    });
-    Spotify.getAlbums(['41MnTivkwTO3UUJ8DrqEJJ','6JWc4iAiJ9FjyK0B59ABb4','6UXCm6bOO4gFlDQZV5yL37']).then(function (data) {
-      console.log('=================== Albums - Array ===================');
-      console.log(data);
-    });
+  }
 
+  $scope.getCurrentUser = function () {
+    $http.get("https://api.spotify.com/v1/me", {
+      headers: {'Authorization': "Bearer " + $scope.token}
+    }).then(function(response) {
+        $scope.currentUser = response.data;
+        return response.data;
+    }, function(response) {
+      console.log(response.data || "Request failed");
+      console.log(response.status);
+  })};
 
-    Spotify.getAlbumTracks('41MnTivkwTO3UUJ8DrqEJJ').then(function (data) {
-      console.log('=================== Album Tracks - ID ===================');
-      console.log(data);
+  $scope.getUserOwnedPlaylists = function() {
+    Spotify.getUserPlaylists($scope.currentUser["id"], {"limit": 50}).then(function (data){
+      $scope.playlists = data.items.filter(function(playlist) {
+        return playlist.owner.id === $scope.currentUser["id"];
+      });
     });
-    Spotify.getAlbumTracks('spotify:album:41MnTivkwTO3UUJ8DrqEJJ').then(function (data) {
-      console.log('=================== Album Tracks - Spotify URI ===================');
-      console.log(data);
-    });
+  }
 
+  $scope.fetchAllTracks = function (playlists) {
+      playlists.forEach( function(p) {
+         $scope.tracks[p.id] = $scope.getPlaylistTracks(p.id);
+      });
+  }
 
+  $scope.deduplicate = function(playlistId) {
 
-    //Artist
-    Spotify.getArtist('0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
-      console.log('=================== Artist - Id ===================');
-      console.log(data);
-    });
-    Spotify.getArtist('spotify:artist:0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
-      console.log('=================== Artist - Spotify URI ===================');
-      console.log(data);
-    });
-
-    Spotify.getArtistAlbums('0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
-      console.log('=================== Artist Albums - Id ===================');
-      console.log(data);
-    });
-
-    Spotify.getArtistAlbums('spotify:artist:0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
-      console.log('=================== Artist Albums - Spotify URI ===================');
-      console.log(data);
-    });
-
-    Spotify.getArtistTopTracks('0LcJLqbBmaGUft1e9Mm8HV', 'AU').then(function (data) {
-      console.log('=================== Artist Top Tracks Australia ===================');
-      console.log(data);
-    });
-
-    Spotify.getRelatedArtists('0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
-      console.log('=================== Get Releated Artists ===================');
-      console.log(data);
-    });
-
-
-    //Tracks
-    Spotify.getTrack('0eGsygTp906u18L0Oimnem').then(function (data) {
-      console.log('=================== Track ===================');
-      console.log(data);
-    });
-
-    Spotify.getTracks('0eGsygTp906u18L0Oimnem,1lDWb6b6ieDQ2xT7ewTC3G').then(function (data) {
-      console.log('=================== Tracks - String ===================');
-      console.log(data);
-    });
-
-    Spotify.getTracks(['0eGsygTp906u18L0Oimnem','1lDWb6b6ieDQ2xT7ewTC3G']).then(function (data) {
-      console.log('=================== Tracks - Array ===================');
-      console.log(data);
-    });
+  }
 
   }]);
+
+app.filter('startFrom', function() {
+  return function(input, start) {
+    start = +start; //parse to int
+    return input.slice(start);
+  }
+});
